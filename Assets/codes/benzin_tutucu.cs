2d9d26b39871777286a124652b3c509f56a5db8c
yusuferol175 <78260853+yusuferol175@users.noreply.github.com>
2023-05-25 12:24:06 +0300

    Update benzin_tutucu.cs

--- Assets/codes/benzin_tutucu.cs
+++ Assets/codes/benzin_tutucu.cs
@@ -11,15 +11,4 @@ public class benzin_tutucu : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
