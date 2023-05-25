ef157123e5126bc5997121097ac8891b0bcf1128
yusuferol175 <78260853+yusuferol175@users.noreply.github.com>
2023-05-25 12:25:05 +0300

    Update cay.cs

--- Assets/codes/cay.cs
+++ Assets/codes/cay.cs
@@ -12,15 +12,4 @@ public class cay : MonoBehaviour
         }
         
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
